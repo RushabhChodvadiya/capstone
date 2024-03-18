e722aeca591cf9ba75aaf980b0cf35e9b7a8d558
Rushabh Chodvadiya <slica.rushabh@gmail.com>
2024-03-18 00:01:44 -0400

    Add dependencies and fetch historical data from Binance API

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const WebSocket = require("ws");
+const axios = require("axios");
+const dayjs = require("dayjs");
+
+const HistoricalDataAPIEndpoint = "http://localhost:3000/data";
 
 // Connect to Binance WebSocket API
 const binanceWS = new WebSocket(
@@ -11,11 +15,106 @@ const wss = new WebSocket.Server({ port: 8080 });
 // Store connected clients
 const clients = new Map(); // Using a Map to store clients and their desired cryptocurrency
 
+async function fetchAndInsertDataForTimeRange(startTime, endTime) {
+  try {
+
+    //https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md#klinecandlestick-data
+    const BTCresponse = await axios.get("https://api.binance.com/api/v3/klines", {
+      params: {
+        symbol: "BTCUSDT",
+        interval: "1m",
+        startTime: startTime.valueOf(), // Convert to Unix timestamp
+        endTime: endTime.valueOf(), // Convert to Unix timestamp
+        limit: 1000, // Adjust as needed, maximum allowed limit
+      },
+    });
+
+    const ETHresponse = await axios.get("https://api.binance.com/api/v3/klines", {
+      params: {
+        symbol: "ETHUSDT",
+        interval: "1m",
+        startTime: startTime.valueOf(), // Convert to Unix timestamp
+        endTime: endTime.valueOf(), // Convert to Unix timestamp
+        limit: 1000, // Adjust as needed, maximum allowed limit
+      },
+    });
+
+    const BTCticks = BTCresponse.data;
+    const ETHticks = ETHresponse.data;
+
+    // Extract necessary data from the response
+    const dataToInsert = BTCticks.map((tick) => {
+      const [timestamp, open, high, low, close, volume] = tick;
+      return [
+        "BTCUSDT", // Example ticker
+        dayjs(timestamp).format("YYYY-MM-DD HH:mm:ss"), // Format timestamp using dayjs
+        parseFloat(open),
+        parseFloat(close),
+        parseFloat(high),
+        parseFloat(low),
+        parseInt(volume),
+      ];
+    });
+
+    const dataToInsertETH = ETHticks.map((tick) => {
+      const [timestamp, open, high, low, close, volume] = tick;
+      return [
+        "ETHUSDT", // Example ticker
+        dayjs(timestamp).format("YYYY-MM-DD HH:mm:ss"), // Format timestamp using dayjs
+        parseFloat(open),
+        parseFloat(close),
+        parseFloat(high),
+        parseFloat(low),
+        parseInt(volume),
+      ];
+    });
+
+    // call the API to insert the data
+    [
+      ...dataToInsert,
+      ...dataToInsertETH,
+    ].forEach(async (data) => {
+      try {
+        await axios.post(HistoricalDataAPIEndpoint, {
+          ticker: data[0],
+          timestamp: data[1],
+          open_price: data[2],
+          close_price: data[3],
+          high_price: data[4],
+          low_price: data[5],
+          volume: data[6],
+        });
+      } catch (error) {
+        console.error("Error inserting data into database:", error);
+      }
+    });
+
+    console.log(
+      "Data to insert: at " + dayjs().format("YYYY-MM-DD HH:mm:ss"),
+      dataToInsert
+    );
+  } catch (error) {
+    console.error("Error fetching data from Binance API:", error);
+  }
+}
+
+// Calculate start time and end time for the previous minute
+let endTime = dayjs().startOf("minute");
+let startTime = endTime.subtract(1, "minute");
+
 // Listen for Binance WebSocket messages
 binanceWS.on("message", (data) => {
   // This handler will only process incoming messages from Binance
   const message = JSON.parse(data);
   const crypto = message.s.startsWith("BTC") ? "BTC" : "ETH";
+  // if currrent time and end time have a diff larger then 1 mint, fetch and insert data for the previous minute
+  if (dayjs().diff(endTime, "minute") > 1) {
+    fetchAndInsertDataForTimeRange(startTime, endTime);
+    // Update start time and end time for the previous minute
+    endTime = dayjs().startOf("minute");
+    startTime = endTime.subtract(1, "minute");
+  }
+
   if (clients.has(crypto)) {
     clients.get(crypto).forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
